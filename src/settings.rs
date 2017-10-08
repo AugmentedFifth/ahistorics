51fc635f58b9676eea8e30a7f9dbf559de4e18ad
Augmented Fourth <zcomito@gmail.com>
2017-10-08 03:03:33 -0700

    weedly

--- src/settings.rs
+++ src/settings.rs
@@ -3,6 +3,7 @@ use graphics::types::Color;
 use toml;
 
 use std::error::Error;
+use std::fmt::{Display, Formatter};
 use std::fs::File;
 use std::io::Read;
 use std::path::Path;
@@ -16,13 +17,25 @@ pub struct Settings {
     player_outline_color: Color,
 }
 
+#[derive(Debug)]
+pub enum SettingsError {
+    Io(String),
+    De(String),
+}
+
 
 impl Settings {
-    pub fn get_from<P: AsRef<Path>, E: From<Error>>(settings_path: P) -> Result<Self, E> {
+    pub fn get_from<P: AsRef<Path>>(settings_path: P) -> Result<Self> {
         let mut settings_file = File::open(settings_path)?;
         let mut contents = String::new();
         settings_file.read_to_string(&mut contents)?;
 
         toml::from_str(contents)
+    }
+}
+
+impl Display for SettingsError {
+    fn fmt(&self, f: &mut Formatter) -> Result<(), Error> {
+        write!(f, "({}, {})", self.x, self.y);
     }
 }
